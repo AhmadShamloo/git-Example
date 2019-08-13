4c51839ab2c26b8ce83ebe59d6dd115ea59b7a57
ahmad shamloofard <a.shamloo2013@chmail.ir>
2019-08-13 12:49:18 +0430

    Add hello.html and Error page

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 
 const app = new express();
 
+app.set('view engine','hbs');
+
+app.use(express.static(path.join(__dirname,'public')));
+// app.use((req,res,next)=>{
+//     res.render('Error');
+// });
 const port=3000;
 
 app.get('/',(req,res)=>{
     res.send('سلام');
 });
+// app.get('/api/sayHello',(req,res)=>{
+//     res.render('hello.html');
+// })
 
 app.listen(port,()=>{
     console.log(`server is runing at ${port}.`);
